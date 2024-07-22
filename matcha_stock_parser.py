766879bbe48c0da573dce9d020cbda5c8aebb0ba
Ben Kagan <ben.kagan01@gmail.com>
2024-07-22 10:49:19 -0400

    put it on a 30 second loop

--- matcha_stock_parser.py
+++ matcha_stock_parser.py
@@ -1,5 +1,7 @@
 import requests
 import smtplib
+import time
+import datetime
 from dotenv import dotenv_values
 from email.message import EmailMessage
 from bs4 import BeautifulSoup
@@ -10,7 +12,7 @@ CARRIERS = {
     "verizon": "@vtext.com",
     "sprint": "@messaging.sprintpcs.com"
 }
-
+SECONDS = 30
 config = dotenv_values(".env")
 
 def get_page_html(url):
@@ -35,6 +37,12 @@ def send_message(phone_number, carrier):
 
 url = "https://www.marukyu-koyamaen.co.jp/english/shop/products/1191040c1/?currency=USD"
 
-html = get_page_html(url)
-# if is_in_stock(html):
-send_message(config["PHONE_NUMBER"], "verizon")
+while(True):
+    output = ""
+    if is_in_stock(get_page_html(url)):
+        send_message(config["PHONE_NUMBER"], "verizon")
+        output = "The product is in stock. Text Message Sent!"
+    else:
+        output = "The product is not in stock. Trying again in " + str(SECONDS) + " seconds."
+    print("["+str(datetime.datetime.now())+"]: " + output)
+    time.sleep(SECONDS)
