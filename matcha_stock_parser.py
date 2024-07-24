7b67558f99fa56d0b133b14c13d8eb319d49a820
Ben Kagan <ben.kagan01@gmail.com>
2024-07-24 11:43:24 -0400

    added matcha names

--- matcha_stock_parser.py
+++ matcha_stock_parser.py
@@ -14,10 +14,14 @@ CARRIERS = {
 }
 SECONDS = 150
 config = dotenv_values(".env")
-URLS = [
-    "https://www.marukyu-koyamaen.co.jp/english/shop/products/1191040c1/?currency=USD",
-    "https://www.marukyu-koyamaen.co.jp/english/shop/products/1161020c1/?currency=USD"
+MATCHA_NAMES = [
+    "Isuzu",
+    "Wako"
 ]
+URLS = {
+    MATCHA_NAMES[0]: "https://www.marukyu-koyamaen.co.jp/english/shop/products/1191040c1/?currency=USD",
+    MATCHA_NAMES[1]: "https://www.marukyu-koyamaen.co.jp/english/shop/products/1161020c1/?currency=USD"
+}
 
 def get_page_html(url):
     page = requests.get(url=url)
@@ -28,7 +32,7 @@ def is_in_stock(html):
     in_stock = soup.find_all("p", {"class": "in-stock"})
     return len(in_stock) > 0
 
-def send_message(phone_number, carrier, url):
+def send_message(phone_number, carrier, matcha_name, url):
     recipient = phone_number + CARRIERS[carrier]
     auth = (config["EMAIL"], config["PASSWORD"])
  
@@ -36,17 +40,17 @@ def send_message(phone_number, carrier, url):
     server.starttls()
     server.login(auth[0], auth[1])
  
-    message = "The matcha is now in stock! Please proceed to " + url
+    message = "The " + matcha_name +" matcha is now in stock! Please proceed to " + url
     server.sendmail(auth[0], recipient, message)
-
 
 i = -1
 while(True):
     output = ""
-    url = URLS[(i + 1) % len(URLS)]
+    i += 1
+    url = URLS[MATCHA_NAMES[i % len(MATCHA_NAMES)]]
     if is_in_stock(get_page_html(url)):
-        send_message(config["PHONE_NUMBER"], "verizon", url)
-        print("The product is in stock. Text Message Sent!")
+        send_message(config["PHONE_NUMBER"], "verizon", MATCHA_NAMES[i], url)
+        print("["+str(datetime.datetime.now())+"]: The " + MATCHA_NAMES[i] + " matcha is in stock. Text Message Sent!")
         break
-    print("["+str(datetime.datetime.now())+"]: The product is not in stock. Trying again in " + str(SECONDS) + " seconds.")
+    print("["+str(datetime.datetime.now())+"]: The " + MATCHA_NAMES[i] + " matcha is not in stock. Checking again in " + str(SECONDS * len(MATCHA_NAMES)) + " seconds.")
     time.sleep(SECONDS)
